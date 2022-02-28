dcac85fa23881f3a0a7d3e36a52fde33ef32fb96
Anurag30112003 <anuragsharma011011@gmail.com>
2022-03-01 03:35:06 +0530

    improve

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,9 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You've successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+      Made with <a href="https://vuejs.org"> Vue</a>
     </h3>
   </div>
 </template>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import About from "@/components/About.vue"
+import  Contact  from "@/components/Contact.vue";
 </script>
 <template>
   <main>
-    <About />
+    <Contact />
   </main>
 </template>
-
